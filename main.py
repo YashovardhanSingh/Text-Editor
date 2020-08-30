172b2b2d194937cc4087dff746f8404e815bec46
yuvalmas <ymashiach2906@gmail.com>
2020-08-30 08:11:32 -0300

    version 0.1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
 
 class Menubar:
@@ -12,26 +13,63 @@ class Menubar:
 
         file_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
         file_dropdown.add_command(label="New File",
+                                  accelerator="Ctrl+N",
                                   command=parent.new_file)
         file_dropdown.add_command(label="Open File",
+                                  accelerator="Ctrl+O",
                                   command=parent.open_file)
         file_dropdown.add_command(label="Save",
+                                  accelerator="Ctrl+S",
                                   command=parent.save)
         file_dropdown.add_command(label="Save As",
+                                  accelerator="Ctrl+Shift+S",
                                   command=parent.save_as)
         file_dropdown.add_separator()
         file_dropdown.add_command(label="Exit",
                                   command=parent.master.destroy)
 
+        about_dropdown = tk.Menu(menubar, font=font_specs, tearoff=0)
+        about_dropdown.add_command(label="Release Notes",
+                                   command=self.show_release_notes)
+        about_dropdown.add_separator()
+        about_dropdown.add_command(label="About",
+                                   command=self.show_about_message)
+
         menubar.add_cascade(label="File", menu=file_dropdown)
+        menubar.add_cascade(label="About", menu=about_dropdown)
+
+    def show_about_message(self):
+        box_title = "About PyText"
+        box_message = "A simple Python Text Editor"
+        messagebox.showinfo(box_title, box_message)
+
+    def show_release_notes(self):
+        box_title = "Release Notes"
+        box_message = "Version 0.1 - Gutenberg"
+        messagebox.showinfo(box_title, box_message)
+
+
+class Statusbar:
+
+    def __init__(self, parent):
+
+        font_specs = ("ubuntu", 12)
+        
+        self.status = tk.StringVar()
+        self.status.set("PyText - Version 0.1")
+
+        label = tk.Label(parent.textarea, textvariable=self.status, fg="black",
+                         bg="lightgrey", anchor='sw', font=font_specs)
+        label.pack(side=tk.BOTTOM, fill=tk.BOTH)
+
+    def update_status(self, *args):
+        if isinstance(args[0], bool):
+            self.status.set("Your File Has Been Saved!")
+        else:
+            self.status.set("PyText - Version 0.1")
 
 
 class PyText:
-    """ PyText is a simple educational Python text editor project.
-    
-    The purpose of PyText is to teach you how to use the TkInter Module.
-	Follow the links in the README file to watch the related tutorial."""
-
 
     def __init__(self, master):
         master.title("Untitled - PyText")
@@ -41,14 +79,17 @@ class PyText:
 
         self.master = master
         self.filename = None
-        
-        self.textarea = tk.Text(master, font=font_specs, bg="black", fg="white")
+
+        self.textarea = tk.Text(master, font=font_specs)
         self.scroll = tk.Scrollbar(master, command=self.textarea.yview)
         self.textarea.configure(yscrollcommand=self.scroll.set)
         self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.scroll.pack(side=tk.RIGHT, fill=tk.Y)
 
         self.menubar = Menubar(self)
+        self.statusbar = Statusbar(self)
+
+        self.bind_shortcuts()
 
     def set_window_title(self, name=None):
         if name:
@@ -56,12 +97,12 @@ class PyText:
         else:
             self.master.title("Untitled - PyText")
 
-    def new_file(self):
+    def new_file(self, *args):
         self.textarea.delete(1.0, tk.END)
         self.filename = None
         self.set_window_title()
 
-    def open_file(self):
+    def open_file(self, *args):
         self.filename = filedialog.askopenfilename(
             defaultextension=".txt",
             filetypes=[("All Files", "*.*"),
@@ -77,37 +118,46 @@ class PyText:
                 self.textarea.insert(1.0, f.read())
             self.set_window_title(self.filename)
     
-    def save(self):
+    def save(self, *args):
         if self.filename:
             try:
                 textarea_content = self.textarea.get(1.0, tk.END)
                 with open(self.filename, "w") as f:
                     f.write(textarea_content)
+                self.statusbar.update_status(True)
             except Exception as e:
                 print(e)
         else:
             self.save_as()
 
-    def save_as(self):
+    def save_as(self, *args):
         try:
             new_file = filedialog.asksaveasfilename(
                 initialfile="Untitled.txt",
                 defaultextension=".txt",
                 filetypes=[("All Files", "*.*"),
-                           ("Text Files", "*.txt"),
-                           ("Python Scripts", "*.py"),
-                           ("Markdown Documents", "*.md"),
-                           ("JavaScript Files", "*.js"),
-                           ("HTML Documents", "*.html"),
-                           ("CSS Documents", "*.css")])
+                        ("Text Files", "*.txt"),
+                        ("Python Scripts", "*.py"),
+                        ("Markdown Documents", "*.md"),
+                        ("JavaScript Files", "*.js"),
+                        ("HTML Documents", "*.html"),
+                        ("CSS Documents", "*.css")])
             textarea_content = self.textarea.get(1.0, tk.END)
             with open(new_file, "w") as f:
                 f.write(textarea_content)
             self.filename = new_file
             self.set_window_title(self.filename)
+            self.statusbar.update_status(True)
         except Exception as e:
             print(e)
 
+    def bind_shortcuts(self):
+        self.textarea.bind('<Control-n>', self.new_file)
+        self.textarea.bind('<Control-o>', self.open_file)
+        self.textarea.bind('<Control-s>', self.save)
+        self.textarea.bind('<Control-S>', self.save_as)
+        self.textarea.bind('<Key>', self.statusbar.update_status)
+        
 
 
 if __name__ == "__main__":
